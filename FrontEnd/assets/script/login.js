f9f014ea556bae4d77bc7bd127655d5584db479f
ThiziriCHEBILI <sahel.thiziri4@gmail.com>
2026-05-21 11:37:10 +0200

    finalisation modale HTML CSS JS

---
+++ FrontEnd/assets/script/login.js
@@ -0,0 +1,32 @@
+async function login() {
+  const email = document.getElementById("email");
+  const password = document.getElementById("password");
+  try {
+    const request = await fetch("http://localhost:5678/api/users/login", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        email: email.value,
+        password: password.value,
+      }),
+    });
+    if (request.ok) {
+      const response = await request.json();
+      if (response) {
+        localStorage.setItem("token", response.token);
+        window.location.href = "index.html";
+      }
+    } else {
+      alert("Email ou mot de passe incorrect !");
+    }
+  } catch (exception) {
+    console.error(`Error: ${exception}`);
+  }
+}
+
+document.querySelector("form").addEventListener("submit", (event) => {
+  event.preventDefault();
+  login();
+});
